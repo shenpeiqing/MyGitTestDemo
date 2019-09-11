e6ddf5ce9dd8fa9b77ac5bde42529d0d378d0125
shen <shenpeiqing168@163.com>
2019-09-11 17:15:57 +0800

    阿斯顿发生发生

--- src/org/westos/demo/MyTest.java
+++ src/org/westos/demo/MyTest.java
@@ -9,5 +9,23 @@ public class MyTest {
     public static void main(String[] args) {
         System.out.println("第一个功能");
         System.out.println("第二个功能");
+        System.out.println("第二个功能");
+        System.out.println("第二个功能");
+        System.out.println("第二个功能");
+        System.out.println("第二个功能");
+        System.out.println("第二个功能");
+        System.out.println("第二个功能");
+        System.out.println("第二个功能");
+        System.out.println("第二个功能");
+        System.out.println("第二个功能");
+
+
+
+
+
+
+
+
+
     }
 }
