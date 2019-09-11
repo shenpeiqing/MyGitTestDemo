7431e1781572ca4e41037fb1ab09310dda78438d
shen <shenpeiqing168@163.com>
2019-09-11 17:06:15 +0800

    第一次提交

---
+++ src/org/westos/demo/MyTest.java
@@ -0,0 +1,12 @@
+package org.westos.demo;
+
+/**
+ * @Author: ShenMouMou
+ * @CreateTime: 2019-09-11 17:03
+ * @Description:西部开源教育科技有限公司
+ */
+public class MyTest {
+    public static void main(String[] args) {
+        System.out.println("第一个功能");
+    }
+}
